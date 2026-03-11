37066b9bcbfacb5ce5f3befe7b05a75b071f3906
Oleksii <oleksii.pedenko@glera-games.com>
2026-03-11 13:48:23 +0200

    add fnc for obj

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,35 @@
  *
  * @return {Object[]}
  */
+
 function transformStateWithClones(state, actions) {
   // write code here
+  let curentState = { ...state };
+  const result = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'clear':
+        curentState = {};
+        break;
+
+      case 'addProperties':
+        curentState = { ...curentState, ...action.extraData };
+        break;
+
+      case 'removeProperties':
+        const nextState = { ...curentState };
+
+        for (const key of action.keysToRemove) {
+          delete nextState[key];
+        }
+        curentState = nextState;
+        break;
+    }
+    result.push(curentState);
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
